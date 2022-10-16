2c01b94cda4dd5c7218db00cbf02d7ad01e29c6b
AvinashGirme <girmeavinash717@gmail.com>
2022-10-16 12:49:54 +0530

    basic folder structure completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages;setup
+
+setup(
+    name = "sensor",
+    version = "0.0.1",
+    author="Avinash Girme",
+    author_email="girmeavinash717@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
